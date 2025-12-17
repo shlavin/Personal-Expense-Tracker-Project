ab8cd66157345ac74878518ff9f2aec08a48c232
Shlavin <nyamburawahu16@gmail.com>
2025-12-17 16:34:24 +0300

    Initialize core Django apps and authentication setup
    - Create users, categories, transactions, reports apps
    - Configure Django REST Framework with Token Authentication
    - Set up MySQL database connection
    - Add initial migrations and project structure
    - Update settings with installed apps and REST framework config

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'users',
+    'categories',
+    'transactions',
+    'reports',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +80,14 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        
+
     }
 }
 
@@ -115,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
